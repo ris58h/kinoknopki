568cb56100060e72eb3013ea3844d83b0950087c
ris58h <ris58h@gmail.com>
2018-06-16 00:11:59 +0300

    Link title.

--- core.js
+++ core.js
@@ -1,18 +1,19 @@
 function createButtons(movieName, movieYear) {
-    const cb = (i, t) => createButton(i, t, movieName, movieYear)
+    const cb = (i, u, t) => createButton(i, u, t, movieName, movieYear)
     
     const buttons = document.createElement("div")
-    buttons.appendChild(cb("http://rutracker.org/favicon.ico", "https://rutracker.org/forum/tracker.php?o=10&s=2&nm=%n %y"))
-    buttons.appendChild(cb("https://thepiratebay.org/favicon.ico", "https://thepiratebay.org/search/%n %y/"))
+    buttons.appendChild(cb("http://rutracker.org/favicon.ico", "https://rutracker.org/forum/tracker.php?o=10&s=2&nm=%n %y", "RuTracker"))
+    buttons.appendChild(cb("https://thepiratebay.org/favicon.ico", "https://thepiratebay.org/search/%n %y/", "ThePirateBay"))
     return buttons
 }
 
-function createButton(iconUrl, urlTemplate, movieName, movieYear) {
+function createButton(iconUrl, urlTemplate, title, movieName, movieYear) {
     const link = document.createElement("a")
     let href = urlTemplate
         .replace("%n", movieName)
         .replace("%y", movieYear)
     link.href = href
+    link.title = title ? title : ""
     link.style.display = "inline-block"
     link.style.width = "24px"
     link.style.height = "24px"
