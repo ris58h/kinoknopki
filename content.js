732738f370b27db17467adc788083d4049055040
ris58h <ris58h@gmail.com>
2018-06-15 12:55:10 +0300

    Sort by seads desc.

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
 const header = document.querySelector('h1')
 
-const knopka = document.createElement("div")
-knopka.style.width = "24px"
-knopka.style.height = "24px"
-knopka.style.background = `url(${chrome.extension.getURL("icons/icon.svg")})`
+const button = document.createElement("div")
+button.style.width = "24px"
+button.style.height = "24px"
+button.style.background = `url(${chrome.extension.getURL("icons/icon.svg")})`
 
 const link = document.createElement("a")
-link.href = "https://rutracker.org/forum/tracker.php?nm=" + header.textContent
-link.appendChild(knopka)
+link.href = "https://rutracker.org/forum/tracker.php?o=10&s=2&nm=" + header.textContent
+link.appendChild(button)
 
 header.parentElement.appendChild(link)
